b24f01eeedae3d78a1a9a1815150a79be98f793b
Itzepsillo94 <kevin.itzep94@gmail.com>
2020-06-01 21:51:49 -0500

    refactorizar TicketRoutes y agregar pay ticket

--- 04_Mongo_Referencias/routes/TicketRoutes.js
+++ 04_Mongo_Referencias/routes/TicketRoutes.js
@@ -15,7 +15,7 @@ router.post('/api/tickets',(req,res)=>{
     newTicket.save()
         .then(dbRes => res.status(201).json(dbRes))
         .catch(err => res.status(404).json(err))
-})
+});
 
 //    Ticket.find().populate('products')
 //READ (ALL)
@@ -24,7 +24,7 @@ router.get('/api/tickets',(req,res)=>{
         .then(tickets => res.status(200).json(tickets))
         .catch(err => res.status(404).json(err));
     
-})
+});
 //READ (ONE)
 router.get('/api/tickets/:id',(req,res)=>{
     Ticket.findById(req.params.id)
@@ -34,14 +34,14 @@ router.get('/api/tickets/:id',(req,res)=>{
             res.status(200).json(tickets)
         })
         .catch(err => res.status(404).json(err));
-})
+});
 
 //UPDATE
 router.patch('/api/tickets/:id',(req,res)=>{
     Ticket.findById(req.params.id,req.body, {new : true})
         .then(updateTicket => res.status(200).json(updateTicket))
         .catch(err => res.status(404).json(err));
-})
+});
 
 //DELETE
 router.delete('/api/tickets/:id',(req,res)=>{
@@ -49,3 +49,29 @@ router.delete('/api/tickets/:id',(req,res)=>{
         .then(updateTicket => res.status(200).json(updateTicket))
         .catch(err => res.status(404).json(err));
 })
+
+
+// PAY TICKET
+router.patch('/api/tickets/:id/pay-ticket',(req,res)=>{
+    Ticket.findById(req.params.id)
+        .populate('products')
+        .then(ticket => {
+            if(!ticket) res.status(404).json({message: 'ticket not found'})
+            const { products } = ticket;
+            const prices = products.map(product => product.price)
+            const subtotal = prices.reduce((total,amount)=>total+amount);
+            const tax = (subtotal*0.16);
+            const total = (tax + subtotal)
+            //res.json({subtotal,total,tax})
+            return Ticket
+            .findByIdAndUpdate(ticket._id,{subtotal,tax,total},{new: true})
+            .populate('products');
+
+        })
+        .then(calculatedTicket => res.status(200).json(calculatedTicket))
+        .catch(err => res.status(404).json(err));
+
+});
+
+
+module.exports = router;
