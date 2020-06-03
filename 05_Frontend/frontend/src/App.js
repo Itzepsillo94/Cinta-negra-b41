a8501082739ef1ef53131666d57a48bb87c63e51
Itzepsillo94 <kevin.itzep94@gmail.com>
2020-06-02 21:10:10 -0500

    Se agrego el formulario para crear un nuevo producto

--- 05_Frontend/frontend/src/App.js
+++ 05_Frontend/frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ProductsList from './components/ProductsList';
+import NewProduct from './components/NewProduct';
 
 function App() {
   return (
     <React.Fragment>
       <h1>Hola Mundo</h1>
       <ProductsList />
+      <NewProduct/>
     </React.Fragment>
   );
 }
