cd8075327f58ef0babdbdb3ba8c0a8738caa88a3
Itzepsillo94 <kevin.itzep94@gmail.com>
2020-06-02 19:46:18 -0500

    se modulo productlist

---
+++ 05_Frontend/frontend/src/components/ProductsList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Product from './Product';
+
+const ProductsList = () => {
+    return (
+        <React.Fragment>
+        <h2>Products</h2>
+        <Product 
+        name="Sopa Nissin" 
+        price={10.50}
+        />
+        </React.Fragment>
+    )
+}
+
+export default ProductsList;
