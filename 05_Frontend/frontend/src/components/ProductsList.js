2d66ad87f8c96bb7112a56b2cf752df50344302d
Itzepsillo94 <kevin.itzep94@gmail.com>
2020-06-02 21:09:52 -0500

    Se agrego axios get  para ver todos los productos

--- 05_Frontend/frontend/src/components/ProductsList.js
+++ 05_Frontend/frontend/src/components/ProductsList.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Product from './Product';
 
+
+const axios = require('axios');
+
 const ProductsList = () => {
+    
+    const [products, setProducts] = useState([])
+
+    useEffect(()=>{
+        axios.get('http://localhost:4040/api/products')
+        .then((res) => setProducts(res.data))
+        .catch((err)=> console.log(err))
+
+    },[])
+
     return (
         <React.Fragment>
         <h2>Products</h2>
+    { products.map(product => 
         <Product 
-        name="Sopa Nissin" 
-        price={10.50}
-        />
+            key={product._id} 
+            name={product.name} 
+            price={product.price}
+        />)}
         </React.Fragment>
     )
 }
