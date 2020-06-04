0b4e7794df2411f598ffbeebd786e0d10f6452ce
Itzepsillo94 <kevin.itzep94@gmail.com>
2020-06-03 19:18:30 -0500

    Se agrego estilo a la lista de producto

--- 05_Frontend/frontend/src/components/NewProduct.js
+++ 05_Frontend/frontend/src/components/NewProduct.js
@@ -6,42 +6,59 @@ const NewProduct = () => {
     const [newName, setName] = useState('');
     const [newPrice, setNewPrice] = useState('');
 
-    const handName = (e) =>{
+    const handName = (e) => {
         setName(e.target.value)
         console.log(e.target.value);
     }
 
-    const handPrice = (e) =>{
+    const handPrice = (e) => {
         setNewPrice(e.target.value)
         console.log(e.target.value);
     }
 
     const createProduct = () => {
-        const body ={
+        const body = {
             name: newName,
             price: newPrice
         }
-        
-        axios.post('http://localhost:4040/api/products',body)
-        .then(()=>console.log("Listo"))
-        .cath(err => console.log(err))
-        
+
+        axios.post('http://localhost:4040/api/products', body)
+            .then(() => console.log("Listo"))
+            .cath(err => console.log(err))
+
     }
 
     return (
-        <div>
-            <form>
-                <label>
-                    Name:
-                <input onChange={handName} type="text" name="name" />
-                </label>
-                <label>
-                    Price:
-                <input onChange={handPrice} type="number" name="name" />
-                </label>
-                <input onClick={()=> createProduct()} type="submit" value="Submit" />
-            </form>
+        <React.Fragment>
+            <article>
+            <div class="col-md-4 mx-auto">
+                <div style={{
+                    backgroundColor: "#5672f0",
+                    padding: "1em",
+                    width: "300px",
+                }}>
+                    <div class="card">
+                        <div class="card-header">
+                            Create Document
+                        </div>
+                    <div class="card-body">
+                        <form>
+                            <div class="form-group">
+                                <input onChange={handName} placeholder="Name" type="text" name="name" />
+                            </div>
+                            <div class="form-group">
+                                <input onChange={handPrice} placeholder="Price" type="number" name="name" />
+                            </div>
+                            <button onClick={() => createProduct()} value="Submit" type="submit" class="btn btn-primary btn-block" >
+                                Create
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
+        </article>
+        </React.Fragment>
     )
 }
 
