d6bb06bcbdde287ba3acb60c912e3563fefbf2d1
Itzepsillo94 <kevin.itzep94@gmail.com>
2020-06-03 21:39:39 -0500

    Se modifica el handle

--- 05_Frontend/frontend/src/components/NewProductForm.js
+++ 05_Frontend/frontend/src/components/NewProductForm.js
@@ -1,67 +1,59 @@
 import React, { useState } from 'react'
 const axios = require('axios');
 
-const NewProduct = () => {
+const NewProduct = (props) => {
 
-    const [newName, setName] = useState('');
-    const [newPrice, setNewPrice] = useState('');
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState(0);
 
-    const handName = (e) => {
-        setName(e.target.value)
-        console.log(e.target.value);
-    }
-
-    const handPrice = (e) => {
-        setNewPrice(e.target.value)
-        console.log(e.target.value);
-    }
-
-    const createProduct = () => {
-        const body = {
-            name: newName,
-            price: newPrice
-        }
-
-        axios.post('http://localhost:4040/api/products', body)
-            .then(() => console.log("Listo"))
-            .cath(err => console.log(err))
-
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        axios.post('http://localhost:4040/api/products',{name,price})
+        .then(()=>axios.get('http://localhost:4040/api/products'))
+        .then((res) => props.setProducts(res.data))
+        .cath(err => console.log(err));
     }
 
     return (
         <React.Fragment>
             <article>
-            <div className="row">
-            <div class="col-md-4 mx-auto">
-                <div style={{
-                    backgroundColor: "#5672f0",
-                    padding: "1em",
-                    width: "300px",
-                }}>
-                    <div class="card mt-4 text-center">
-                    <div class="card">
-                        <div class="card-header">
-                            Create Document
+                <div className="row">
+                    <div className="col-md-4 mx-auto">
+                        <div style={{
+                            backgroundColor: "#5672f0",
+                            padding: "1em",
+                            width: "300px",
+                        }}>
+                            <div className="card mt-4 text-center">
+                                <div className="card">
+                                    <div className="card-header">
+                                        Create Document
+                                    </div>
+                                    <div className="card-body">
+                                        <form 
+                                            onSubmit={handleSubmit}
+                                            style={{
+                                            display: "flex",
+                                            flexDirection: "column",
+                                            justifyContent: "center",
+                                        }}>
+                                            <div className="form-group">
+                                                <input onChange={(e)=> setName(e.target.value)}  style={{ marginBottom: "1em"}} placeholder="Name" type="text" value={name} />
+                                            </div>
+                                            <div className="form-group">
+                                                <input onChange={(e)=> setPrice(e.target.value)} style={{ marginBottom: "1em"}} placeholder="Price" type="number" value={price} />
+                                            </div>
+                                            <button  type="submit" class="btn btn-primary btn-block" >
+                                                Create
+                                            </button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    <div class="card-body">
-                        <form>
-                            <div class="form-group">
-                                <input onChange={handName} placeholder="Name" type="text" name="name" />
-                            </div>
-                            <div class="form-group">
-                                <input onChange={handPrice} placeholder="Price" type="number" name="name" />
-                            </div>
-                            <button onClick={() => createProduct()} value="Submit" type="submit" class="btn btn-primary btn-block" >
-                                Create
-                            </button>
-                        </form>
                     </div>
                 </div>
-            </div>
-            </div>
-            </div>
-        </div>
-        </article>
+            </article>
         </React.Fragment>
     )
 }
