9cce2b338c6c9d4ce7e5ca6f33946977f76a19c5
Itzepsillo94 <kevin.itzep94@gmail.com>
2020-06-03 21:39:16 -0500

    Se quita el componente newproduct

--- 05_Frontend/frontend/src/App.js
+++ 05_Frontend/frontend/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ProductsList from './components/ProductsList';
-import NewProductForm from './components/NewProductForm';
 
 function App() {
   return (
     <React.Fragment>
       <h1>Hola Mundo</h1>
       <ProductsList />
-      <NewProductForm/>
     </React.Fragment>
   );
 }
